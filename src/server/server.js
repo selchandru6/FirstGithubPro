2e6dda301b6a07d6a6d9bb8e357da72ea26c809b
Sellappan <327912@Cognizant.com>
2016-06-18 15:25:42 -0400

    Commit updated.

--- src/server/server.js
+++ src/server/server.js
@@ -5,6 +5,9 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -20,11 +23,56 @@ var router = express.Router();              // get an instance of the express Ro
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
 	console.log('I can write my logic over here');
+	url = 'http://www.imdb.com/title/tt1229340/';
+
+request(url, function(error, response, html){
+
+	console.log('inside the request');
 	
-    res.json({ message: 'hooray! welcome to our api!' ,id:'321654987',name:'Testing API'}
+	console.log('inside the request error'+error);
+	console.log('inside the request response'+response);
+	console.log('inside the request html'+html);
+	
 
+    if(!error){
+        var $ = cheerio.load(html);
 
-    	);   
+    var title, release, rating;
+    var json = { title : "", release : "", rating : ""};
+
+    $('.header').filter(function(){
+        var data = $(this);
+        title = data.children().first().text();            
+        release = data.children().last().children().text();
+
+        json.title = title;
+        json.release = release;
+    })
+
+    $('.star-box-giga-star').filter(function(){
+        var data = $(this);
+        rating = data.text();
+
+        json.rating = rating;
+    })
+}
+
+// To write to the system we will use the built in 'fs' library.
+// In this example we will pass 3 parameters to the writeFile function
+// Parameter 1 :  output.json - this is what the created filename will be called
+// Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
+// Parameter 3 :  callback function - a callback function to let us know the status of our function
+
+fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+
+    console.log('File successfully written! - Check your project directory for the output.json file');
+
+})
+
+// Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+res.send('Check your console!')
+
+    }) ;
 });
 
 // more routes for our API will happen here
